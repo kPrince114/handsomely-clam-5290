b4b33b8f471babe666c842f0b9781f3fe1f89fe9
kPrince114 <princep7277789921@gmail.com>
2022-10-02 01:03:37 +0530

    day5

---
+++ scripts/explore.js
@@ -0,0 +1,45 @@
+import navbar from "../components/navbar.js";
+console.log(navbar)
+
+let navbar_div = document.getElementById("navbar");
+navbar_div.innerHTML=navbar();
+
+let data1 = JSON.parse(localStorage.getItem("news"));
+    console.log(data1);
+    let maindiv = document.getElementById("newsmain")
+    let tag=document.getElementById("tag");
+    let data=data1[0];
+    let appendProducts = (data) => {
+        maindiv.innerHTML = null;
+        tag.innerHTML=null;
+            let div1 = document.createElement("div");
+            let img = document.createElement('img');
+            img.src = data.urlToImage;
+            let div2 = document.createElement("div");
+            let content=document.createElement("p");
+            content.innerText=data.content;
+            let title = document.createElement('h4');
+            title.innerText = data.title;
+            let publishedAt = document.createElement('h5');
+            let bag = ""
+            for (let i = 0; i < 10; i++) {
+                bag = bag + data.publishedAt[i]
+            }
+            publishedAt.innerText ="Posted date:- "+bag;
+            let div3 = document.createElement("div")
+            div2.append(publishedAt,img,content);
+            tag.append(title);
+            div1.append(div2);
+            div1.setAttribute("class","news");
+            // div1.addEventListener("click", function () {
+            //     let array1 = []
+            //     array1.push(ele)
+            //     localStorage.setItem("news", JSON.stringify(array1))
+            //     window.location.href = "drop.html";
+            // });
+            maindiv.append(div1);
+    }
+    appendProducts(data);
+    function clickfun1(){
+        window.location.href="https://www.wristmate.in/pages/smartwatch?utm_source=taboola&utm_campaign=20299339&utm_medium=deccanherald.com&utm_term=http%3A%2F%2Fcdn.taboola.com%2Flibtrc%2Fstatic%2Fthumbnails%2Fb743b4cef8d4d7d71e6e2ec05e19a513.png%20&utm_content=India%3A+Why+is+Everyone+Excited+Over+This+Rs.+2499+Smartwatch&tblci=GiB2DBk1KH2J_zK1UbfRf_rOep_IBygP0qmtXiOXTzhpuiCHrlEo5PiomfO2gsbFAQ#tblciGiB2DBk1KH2J_zK1UbfRf_rOep_IBygP0qmtXiOXTzhpuiCHrlEo5PiomfO2gsbFAQ"
+    }
